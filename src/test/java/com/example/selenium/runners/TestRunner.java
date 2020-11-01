d151208bf4ee4e3ace5e976c65eb2aa3166d226a
christianAA <christianaagit@gmail.com>
2020-11-01 12:09:33 +0100

    Changing reporting methods

--- src/test/java/com/example/selenium/runners/TestRunner.java
+++ src/test/java/com/example/selenium/runners/TestRunner.java
@@ -8,7 +8,7 @@ import org.junit.runner.RunWith;
 @RunWith(Cucumber.class)
 @CucumberOptions(
         features = "classpath:features",
-        plugin = {"pretty", "html:results/smth", "json:results/cucumber.json", "junit:results/cucumber.xml"},
+        plugin = {"pretty", "html:results/cucumber-reports.html", "json:results/cucumber.json", "junit:results/cucumber.xml"},
         glue = "com.example.selenium",
         tags = "not @wip")
 
